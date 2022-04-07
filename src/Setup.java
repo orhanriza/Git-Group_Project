f8946986af055e8f3a670d813d8ebb2cfa474e9f
jeyhun <jeyhun89>
2022-04-07 14:35:10 -0400

    i put my second comment in the class

--- src/Setup.java
+++ src/Setup.java
@@ -1,12 +1,12 @@
 public class Setup {
     //Setting up project for everyone
 
- develop
     // sikandar made changes
 
     // made another chnage
     //adding new line by Jeyhun
 
     // adding a new information
- master
+
+    // i deleted some illegal words
 }
