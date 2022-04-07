9292fd2c0b5298e1d620bd3d81a272f6ee3e3852
jeyhun <jeyhun89>
2022-04-07 14:45:57 -0400

    third changing from jeyhun

--- src/Setup.java
+++ src/Setup.java
@@ -1,12 +1,11 @@
 public class Setup {
     //Setting up project for everyone
 
- develop
     // sikandar made changes
 
     // made another chnage
     //adding new line by Jeyhun
 
     // adding a new information
- master
+
 }
